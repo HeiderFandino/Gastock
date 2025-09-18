1b5ee153c601df4754d1cc869f2ff04bf668056a
Heider Fandiño <Heiderfandino@gmail.com>
2025-09-18 17:33:18 +0200

    clean up CSS spacing and finalize fixes

--- render_build.sh
+++ render_build.sh
@@ -8,9 +8,4 @@ npm run build
 pip install pipenv
 pipenv install
 
-# Marcar migración como aplicada sin ejecutarla (tablas ya existen)
-echo "=== Marcando migración como aplicada ==="
-pipenv run flask db stamp 2e5db5dafba1
-
-echo "=== Verificando estado final ==="
-pipenv run flask db current
+pipenv run upgrade
