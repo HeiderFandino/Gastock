2f4a3468f6edc4a48b88e935435a00b19de3657c
Heider Fandiño <Heiderfandino@gmail.com>
2025-09-18 17:39:56 +0200

    clean restart: baseline migration and stable codebase

---
+++ migrations/versions/7b25a2a2abae_baseline_migration.py
@@ -0,0 +1,24 @@
+"""baseline migration
+
+Revision ID: 7b25a2a2abae
+Revises: 
+Create Date: 2025-09-18 17:39:09.363194
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7b25a2a2abae'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
