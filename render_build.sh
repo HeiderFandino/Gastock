c744a1b2850e2bdecf1bdba3064395464e02bcc5
Heider Fandiño <Heiderfandino@gmail.com>
2025-09-18 17:01:15 +0200

    fix build order - migrations before frontend

--- render_build.sh
+++ render_build.sh
@@ -2,11 +2,11 @@
 # exit on error
 set -o errexit
 
-npm install
-npm run build
-
 pip install pipenv
 pipenv install
 
-# Limpiar migraciones problemáticas
+# Limpiar migraciones problemáticas ANTES del frontend
 python reset_migrations.py
+
+npm install
+npm run build
